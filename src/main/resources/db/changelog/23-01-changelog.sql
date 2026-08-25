d50dd1d14c3bca6d0973f0a9b460213156f4b473
Felipe Lopes <lopes.felipe@tjrj.jus.br>
2026-08-25 13:46:15 -0300

    update documentação

--- src/main/resources/db/changelog/23-01-changelog.sql
+++
@@ -1,9 +0,0 @@
--- liquibase formatted sql
-
--- changeset felipe:1787456631909-2
-ALTER TABLE carro
-    ADD cor VARCHAR(255) NULL;
-
--- changeset felipe:1787456631909-3
-DROP TABLE revinfo_seq;
-
